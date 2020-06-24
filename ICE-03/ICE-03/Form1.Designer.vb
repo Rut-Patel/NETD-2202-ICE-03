d338f6862403dc1f4c85fb44b6ac79a64e0b7474
Rut Patel <patelrut2001@gmail.com>
2020-06-24 14:49:37 -0400

    First Commit

---
+++ ICE-03/ICE-03/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmGreet
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnAccept = New System.Windows.Forms.Button()
+        Me.BtnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnAccept
+        '
+        Me.btnAccept.Location = New System.Drawing.Point(54, 66)
+        Me.btnAccept.Name = "btnAccept"
+        Me.btnAccept.Size = New System.Drawing.Size(75, 23)
+        Me.btnAccept.TabIndex = 0
+        Me.btnAccept.Text = "&Click"
+        Me.btnAccept.UseVisualStyleBackColor = True
+        '
+        'BtnExit
+        '
+        Me.BtnExit.Location = New System.Drawing.Point(149, 66)
+        Me.BtnExit.Name = "BtnExit"
+        Me.BtnExit.Size = New System.Drawing.Size(75, 23)
+        Me.BtnExit.TabIndex = 1
+        Me.BtnExit.Text = "E&xit"
+        Me.BtnExit.UseVisualStyleBackColor = True
+        '
+        'frmGreet
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(271, 130)
+        Me.Controls.Add(Me.BtnExit)
+        Me.Controls.Add(Me.btnAccept)
+        Me.Name = "frmGreet"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Greeter"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnAccept As Button
+    Friend WithEvents BtnExit As Button
+End Class
---
+++ ICE-03/ICE-03/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class frmGreet
+    Private Sub btnAccept_Click(sender As Object, e As EventArgs) Handles btnAccept.Click
+        MessageBox.Show("You are most welcome..............!!!!!")
+    End Sub
+
+    Private Sub BtnExit_Click(sender As Object, e As EventArgs) Handles BtnExit.Click
+        MessageBox.Show("Have great day..............!!!!!")
+    End Sub
+End Class
